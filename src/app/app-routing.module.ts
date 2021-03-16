09e8dc0d9bc80538686b6710c6db70d23c71ed6a
vladimir.makhmutov <vladimir.makhmutov@indusoft.ru>
2021-03-16 15:05:31 +0700

    Commit before update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OrderStatusComponent } from './order-status/order-status.component';
+import { OrderDeliveryComponent } from './order-delivery/order-delivery.component';
+import { FlashMobComponent } from './fm/flash-mob.component';
+import {D3ChartComponent} from './d3-chart/d3-chart.component';
+
+
+const routes: Routes = [
+    { path: 'd3-chart', component: D3ChartComponent },
+    { path: 'status', component: OrderStatusComponent },
+    { path: 'delivery', component: OrderDeliveryComponent },
+    { path: 'flashmob', component: FlashMobComponent },
+    { path: '', component: D3ChartComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChartControlsService } from './chart-controls.service';
+
+// https://stackoverflow.com/questions/38087013/angular2-web-speech-api-voice-recognition
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+  webkitAudioContext: any;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Using d3 within Angular 11';
+
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    public chartControlsService: ChartControlsService) { }
+
+  ngOnInit() {
+  }
+
+  navigate(path) {
+    this.ngZone.run(() => {
+      this.router.navigate([path]);
+    });
+
+  }
+
+}
